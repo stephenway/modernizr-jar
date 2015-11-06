daaa74a1342261b8be606cfc06bccf3f44418ee4
Stephen Way <scway@thenavisway.com>
2015-11-06 15:18:25 -0800

    Clojure project and ignore build files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject modernizr "3.2.0"
+  :description "Modernizr is a JavaScript library that detects HTML5 and CSS3 features in the user’s browser."
+  :url "https://github.com/Modernizr/Modernizr"
+  :license {:name "MIT"
+            :url "https://raw.githubusercontent.com/Modernizr/Modernizr/master/LICENSE"}
+  :dependencies [])
